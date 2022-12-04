9c2220e7542b84487544dcb920a7c3911236a273
Makarov Artem <rex0ne@mail.ru>
2022-12-05 02:02:50 +0300

    Добавлен файл main.py верхний регистр

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import random
 
 def password():
     one = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    two = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
     otvet = ''
     for i in range(11):
-        otvet += random.choice(one)
+        otvet += random.choice(one + two)
     return otvet
 
 
